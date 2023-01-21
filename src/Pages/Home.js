83d6aad198d2ed8a041616bbf4205b76187d68ca
jalal.seu.cse <jalal.seu.cse@gmail.com>
2023-01-22 00:07:04 +0600

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {Button, Card, Checkbox, Form, Input, Select} from "antd";
 import {DataContenxt} from "../Context/DataContextProvider";
 import {Link} from "react-router-dom";
@@ -8,24 +8,16 @@ import {sectorsData} from "../Data/SectorsData";
 const Home = () => {
 
     const dataContext = useContext(DataContenxt);
-    const [agree, setAgree] = useState(false);
-
-
-    const onAgreeChange = (e) => {
-        setAgree(e.target.checked)
-    }
 
 
     const onFinish = (values) => {
 
-        const _data = {...values, agree};
-
-        console.log('values', values)
-
-        dataContext.setData(_data);
-
+        dataContext.setData(values);
+        alert('Successfully Save Data');
 
     }
+
+
 
     const onFinishFailed = errorInfo => {
         console.log('Failed:', errorInfo);
@@ -47,6 +39,7 @@ const Home = () => {
                         initialValues={dataContext.datas ?
                             {
                                 ...dataContext.datas,
+                                agree: dataContext.datas?.agree
                             } : {}
                         }
                         onFinish={onFinish}
@@ -58,71 +51,42 @@ const Home = () => {
                         <Form.Item
                             label="Name"
                             name="name"
-                            rules={[{required: true, message: 'Please input title'}]}
+                            rules={[{required: true, message: 'Please input name'}]}
                         >
                             <Input/>
                         </Form.Item>
 
-                        {/*<Form.Item*/}
-                        {/*    label="Sectors"*/}
-                        {/*    name="sectors"*/}
-                        {/*    rules={[{required: true, message: 'Please input title'}]}*/}
-                        {/*>*/}
-
-                        {/*<Select*/}
-                        {/*    labelInValue*/}
-                        {/*    defaultValue={{ value: dataContext?.datas?.sectors }}*/}
-                        {/*    style={{width: '100%'}}*/}
-                        {/*    // onChange={onAgreeChange}*/}
-
-                        {/*    options={[*/}
-                        {/*        {*/}
-                        {/*            label: "Manufacturing",*/}
-                        {/*            options:*/}
-                        {/*                [*/}
-                        {/*                    { label: 'Construction', value: 'Construction', id: '1' },*/}
-                        {/*                    { label: 'Electronics and Optics', value: 'Electronics and Optics', id: '2' },*/}
-                        {/*                    {*/}
-                        {/*                        label: "Food and Beverage",*/}
-                        {/*                        options:*/}
-                        {/*                            [*/}
-                        {/*                                { label: 'Bakery & confectionery products ', value: 'Bakery & confectionery products', id: '3' },*/}
-                        {/*                                { label: 'Fish & fish products', value: 'Fish & fish products', id: '4' },*/}
-                        {/*                            ]*/}
-                        {/*                    }*/}
-                        {/*                ]*/}
-                        {/*        }*/}
-                        {/*    ]}*/}
-                        {/*/>*/}
-                        {/*</Form.Item>*/}
-
                         <Form.Item
                             label="Sectors"
                             name="sectors"
-                            rules={[{required: true, message: 'Please input title'}]}
+                            rules={[{required: true, message: 'Please input Sectors'}]}
                         >
                             <Select
-                                defaultValue="Daily" style={{width: '100%'}}
-                                // onChange={onAgreeChange}
+                                placeholder='Select Sector'
+                                style={{width: '100%'}}
                             >
                                 {
                                     sectorsData?.map((value, i) => (
                                         <Option key={i} value={value}>{value}</Option>
                                     ))
                                 }
-                                {/*<Option value="DAILY">Daily</Option>*/}
-                                {/*<Option value="WEEKLY">Weekly</Option>*/}
-                                {/*<Option value="MONTHLY">Monthly</Option>*/}
-                                {/*<Option value="YEARLY">Yearly</Option>*/}
                             </Select>
                         </Form.Item>
 
-                        <div>
-                            <Checkbox
-                                defaultChecked={dataContext.datas?.agree}
-                                onChange={onAgreeChange}
-                            >Agree to terms</Checkbox>
-                        </div>
+                        <Form.Item
+                            name="agree"
+                            valuePropName="checked"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "You need to agree the terms and conditions",
+                                },
+                            ]}
+                        >
+                            <Checkbox  >
+                                Agree to terms
+                            </Checkbox>
+                        </Form.Item>
 
                        <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '10px'}}>
                            <Button
